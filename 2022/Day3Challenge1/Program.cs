d569f8ec4795d8d580cfdb1b20ba34909ad7ed93
Andrew Palmer <andrewpalmer@microsoft.com>
2022-12-13 14:11:04 -0800

    Solved 2022Day3Challenge1

--- 2022/Day3Challenge1/Program.cs
+++ 2022/Day3Challenge1/Program.cs
@@ -2,6 +2,9 @@
 //Make a method that returns a Generic list, use LINQ to analyze generic list
 
 
+using System.Globalization;
+using System.Xml;
+
 string path = "D:\\LocalRepos\\AdventOfCode\\2022\\Day3Challenge1\\data\\input.txt";
 var list = CreateList(path);
 //Now Linq statements
@@ -12,6 +15,7 @@ var listLinq = list.Where((s, n) => n < (s.Length / 2)).Select((s, n) => s[n]);
 var listLinq = list.Select(s => s.Split(s[s.Length / 2], 2));
 */
 
+/*
 //This block of code splits every string in half and prints the first half.
 var listLinq = list.Select(s => s.Substring(0, (s.Length / 2)));
 Console.WriteLine($"Number of items in list 1: {listLinq.Count()}");
@@ -33,6 +37,39 @@ foreach (var item in listLinq3)
 {
     Console.WriteLine(item);
 }
+*/
+
+/*List<string> result = list.Select(s => s.Substring(0, s.Length / 2)
+                                        .Intersect(s.Substring(s.Length / 2))
+                                        .ToList())
+                            .ToList();
+*/
+var result = list.Select(s => s.Substring(0, s.Length / 2)
+                                        .Intersect(s.Substring(s.Length / 2))
+                                        .ToList());
+
+int count = 0;
+List<int> counter = new List<int>();
+foreach (var item in result)
+{
+    if (item[0] > 96)
+    {
+        //Console.WriteLine($"{item[0]}: {item[0] - 96}");
+        counter.Add(item[0] - 96);
+    }
+    else if (item[0] > 64)
+    {
+        //Console.WriteLine($"{item[0]}: {item[0] - 38}");
+        counter.Add(item[0] - 38);
+    }
+    else
+    {
+        Console.WriteLine("Error");
+        count++;
+    }
+}
+Console.WriteLine(count);
+Console.WriteLine(counter.Sum());
 
 /*
 //Nice Try
