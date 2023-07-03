7f6883f82040995668d57e896874a5a0f3ad8e35
Andrew Palmer <andrewpalmer@microsoft.com>
2023-07-03 14:39:13 -0700

    Day 8 Challenge 1 Complete

--- 2022/Day8/Challenge1.cs
+++ 2022/Day8/Challenge1.cs
@@ -19,33 +19,95 @@ public class Challenge1
     {
         List<List<(bool, int)>> data = Utility.RetrieveData();
         //Utility.PrintTupleList(data);
-        int columnCount = 0;
-        int rowCount = 0;
+        data = SolveLeftToRightAndTopToBottom(data);
+        data = SolveRightToLeftAndBottomToTop(data);
 
-        foreach (var item in data)
+        int boolCount = data.SelectMany(x => x).Where(x => x.Item1 == true).Count();
+        Console.WriteLine($"There are {boolCount} visible trees");
+        Console.ReadKey();
+    }
+
+    public static List<List<(bool, int)>> SolveLeftToRightAndTopToBottom(List<List<(bool, int)>> data)
+    {
+        int NumberOfRows = data.Count;
+        int NumberOfColumns = data[0].Count;
+
+        List<int> MaxColumnHeight = data[0].Select(x => x.Item2).ToList();
+
+        for (int rowCount = 0; rowCount < NumberOfRows; rowCount++)
         {
-            foreach (var item2 in item)
+            int rowMaxHeight = data[rowCount][0].Item2;
+
+            for (int c = 0; c < NumberOfColumns; c++)
             {
-                if (item2.Item1 == true)
+                if (data[rowCount][c].Item1 == true)
                 {
                     Console.WriteLine("Skip");
-                    columnCount++;
                     continue;
                 }
-                columnCount++;
 
-                if (item2.Item2 >= data[rowCount][columnCount].Item2)
+                if (data[rowCount][c].Item2 > rowMaxHeight)
                 {
-                    (bool, int) tempTuple = (true, item2.Item2);
+                    rowMaxHeight = data[rowCount][c].Item2;
+                    (bool, int) tempTuple = (true, data[rowCount][c].Item2);
 
-                    data[rowCount][columnCount] = tempTuple;
+                    data[rowCount][c] = tempTuple;
+
+                    Console.WriteLine($"Left to Right: Replaced Row: {rowCount} and Column: {c}");
                 }
 
-            }
+                if (data[rowCount][c].Item2 > MaxColumnHeight[c])
+                {
+                    MaxColumnHeight[c] = data[rowCount][c].Item2;
+                    (bool, int) tempTuple = (true, data[rowCount][c].Item2);
 
-            rowCount++;
+                    data[rowCount][c] = tempTuple;
+
+                    Console.WriteLine($"Top Down: Replaced Row: {rowCount} and Column: {c}");
+
+                }
+            }
         }
 
-        Console.ReadKey();
+        return data;
+    }
+
+    public static List<List<(bool, int)>> SolveRightToLeftAndBottomToTop(List<List<(bool, int)>> data)
+    {
+        int NumberOfRows = data.Count - 1;
+        int NumberOfColumns = data[0].Count - 1;
+
+        List<int> MaxColumnHeight = data[NumberOfRows].Select(x => x.Item2).ToList();
+
+        for (int rowCount = NumberOfRows - 1; rowCount > 0; rowCount--)
+        {
+            int rowMaxHeight = data[rowCount][NumberOfColumns].Item2;
+
+            for (int columnCount = NumberOfColumns; columnCount > 0; columnCount--)
+            {
+                if (data[rowCount][columnCount].Item2 > rowMaxHeight)
+                {
+                    rowMaxHeight = data[rowCount][columnCount].Item2;
+                    (bool, int) tempTuple = (true, data[rowCount][columnCount].Item2);
+
+                    data[rowCount][columnCount] = tempTuple;
+
+                    Console.WriteLine($"Left to Right: Replaced Row: {rowCount} and Column: {columnCount}");
+                }
+
+                if (data[rowCount][columnCount].Item2 > MaxColumnHeight[columnCount])
+                {
+                    MaxColumnHeight[columnCount] = data[rowCount][columnCount].Item2;
+                    (bool, int) tempTuple = (true, data[rowCount][columnCount].Item2);
+
+                    data[rowCount][columnCount] = tempTuple;
+
+                    Console.WriteLine($"Top Down: Replaced Row: {rowCount} and Column: {columnCount}");
+
+                }
+            }
+        }
+
+        return data;
     }
 }
