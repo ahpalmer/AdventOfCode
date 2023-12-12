94e0cf48c98b415951a66cfc8a5f4537622e2e67
Andrew Palmer <ahpalmer72@gmail.com>
2023-12-12 11:24:16 -0500

    Completed Day 6 Chal 2

---
+++ 2023/Day6/Challenge2.cs
@@ -0,0 +1,71 @@
+﻿using Advent2023Utility;
+using System.Text.RegularExpressions;
+
+namespace Advent2023Day6;
+
+internal class Challenge2
+{
+    Regex regex = new Regex(@"(\d+)");
+
+    internal long Solve()
+    {
+        List<string> stringInput = Utility.CreateStringList();
+        var timeDistanceList = CreateTimeDistanceList(stringInput);
+        List<long> answerList = new List<long>();
+
+        foreach (var timeDistance in timeDistanceList)
+        {
+            (long, long) upperLowerBounds = QuadraticFormula(timeDistance.Item1, timeDistance.Item2);
+            answerList.Add(FindSpan(upperLowerBounds));
+        }
+
+        long answer = answerList.Aggregate((workingTotal, next) => (workingTotal != 0) ? workingTotal * next : next);
+        return answer;
+    }
+
+    internal List<(long, long)> CreateTimeDistanceList(List<string> stringInput)
+    {
+        string time = "";
+        string distance = "";
+        List<(long, long)> timeDistanceList = new List<(long, long)>();
+
+        bool check = false;
+        foreach (var input in stringInput)
+        {
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                if (!check)
+                {
+                    time = time + match.Value.ToString();
+                }
+                else
+                {
+                    distance = distance + match.Value.ToString();
+                }
+
+            }
+            check = true;
+        }
+
+        timeDistanceList.Add((Int64.Parse(time), Int64.Parse(distance)));
+        return timeDistanceList;
+    }
+
+    internal long FindSpan((long, long) xBound)
+    {
+        return xBound.Item1 - xBound.Item2 + 1;
+    }
+
+    internal (long, long) QuadraticFormula(long timeAvailableB, long distanceC)
+    {
+        double a = 1;
+        double bSquared = timeAvailableB * timeAvailableB;
+        double fourAC = 4 * a * distanceC;
+        double squareRoot = Math.Sqrt(bSquared - fourAC);
+        long xUpper = (long)Math.Floor((timeAvailableB + squareRoot) / 2);
+        long xLower = (long)Math.Ceiling((timeAvailableB - squareRoot) / 2);
+
+        return (xUpper, xLower);
+    }
+}
--- 2023/Day6/Day6.cs
+++ 2023/Day6/Day6.cs
@@ -1,12 +1,13 @@
 ﻿namespace Advent2023Day6;
 
+//Time: 1.5 hours.  Took me a long time to code but I found a much better solution than brute-forcing the answer
 class Program
 {
     public static void Main(string[] args)
     {
         Challenge1 chal1 = new Challenge1();
         Console.WriteLine(chal1.Solve());
-        //Challenge2 chal2 = new Challenge2();
-        //Console.WriteLine(chal2.Solve());
+        Challenge2 chal2 = new Challenge2();
+        Console.WriteLine(chal2.Solve());
     }
 }
