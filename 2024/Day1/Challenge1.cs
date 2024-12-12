d72120e0deff19a021dea8b3cea1671b1f1354db
Andrew Palmer <ahpalmer72@gmail.com>
2024-12-12 11:11:50 -0500

    Completed Day1Challenge1 2024

--- 2024/Day1/Challenge1.cs
+++ 2024/Day1/Challenge1.cs
@@ -1,10 +1,18 @@
-﻿namespace Day1;
+﻿using Advent2024Utility;
+
+namespace Day1;
 
 public class Challenge1
 {
     public string Solve()
     {
-        Console.WriteLine("Dependency injection works");
-        return "Dependency injection works";
+        List<string> inputList = Utility.CreateStringList("Day1.txt");
+
+        List<int> leftList = inputList.Select(x => x.Split(' ')[0]).Select(s => Int32.Parse(s)).Order().ToList();
+        List<int> rightList = inputList.Select(x => x.Split(' ')[1]).Select(s => Int32.Parse(s)).Order().ToList();
+
+        int difference = leftList.Zip(rightList, (left, right) => Math.Abs(left - right)).Sum();
+        Console.WriteLine(difference.ToString());
+        return difference.ToString();
     }
 }
