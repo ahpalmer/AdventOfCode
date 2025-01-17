ad76be939c61c93a85082c32c67aaae8a6593d78
Andrew Palmer <ahpalmer72@gmail.com>
2025-01-17 12:16:08 -0500

    Completed 2024 DAy4 Chal2

--- 2024/Day4/Challenge2.cs
+++ 2024/Day4/Challenge2.cs
@@ -2,9 +2,51 @@
 
 public class Challenge2
 {
-    //public string Solve()
-    //{
+    private static readonly (int dx, int dy, int direction)[] directions = new[]
+    {
+        (-1, -1, 0), (-1, 1, 1),  // Top left,top right
+        (1, -1, 2),  (1, 1, 3)    // Bottom left, bottom right
+    };
 
-    //}
+    public string Solve(List<string> inputList)
+    {
+        int yLength = inputList.Count;
+        int xLength = inputList[0].Count();
+        int totalXmas = 0;
 
+        for (int y = 0; y < yLength; y++)
+        {
+            for (int x = 0; x < xLength; x++)
+            {
+                if (inputList[y][x] == 'A')
+                {
+                    if (CheckForMASX(inputList, x, y)) { totalXmas++; }
+                }
+            }
+        }
+
+        Console.WriteLine(totalXmas.ToString());
+        return totalXmas.ToString();
+    }
+
+    public bool CheckForMASX(List<string> inputList, int x, int y)
+    {
+        if (y - 1 >= 0 && x - 1 >= 0 && y + 1 < inputList.Count && x + 1 < inputList[0].Count())
+        {
+            char upperLeft = inputList[y - 1][x - 1];
+            char upperRight = inputList[y - 1][x + 1];
+            char lowerLeft = inputList[y + 1][x - 1];
+            char lowerRight = inputList[y + 1][x + 1];
+            if ((upperLeft == 'M' && lowerRight == 'S') || (upperLeft == 'S' && lowerRight == 'M'))
+            {
+                if ((upperRight == 'M' && lowerLeft == 'S') || (upperRight == 'S' && lowerLeft == 'M'))
+                {
+                    return true;
+                }
+            }
+        }
+
+
+        return false;
+    }
 }
--- 2024/Day4/Day4Program.cs
+++ 2024/Day4/Day4Program.cs
@@ -8,13 +8,14 @@ public class Day4Program : IRunProgram
     public async Task<List<string>> Run()
     {
         List<string> inputList = Utility.CreateStringList("Day4.txt");
-        Day4.Challenge1 chal1 = new Challenge1();
-        var task1 = Task.Run(() => chal1.Solve(inputList));
-        var results = (await Task.WhenAll(task1)).ToList();
 
-        //Day4.Challenge2 chal2 = new Challenge2();
-        //var task2 = Task.Run(() => chal2.Solve());
-        //var results = (await Task.WhenAll(task2)).ToList();
+        //Day4.Challenge1 chal1 = new Challenge1();
+        //var task1 = Task.Run(() => chal1.Solve(inputList));
+        //var results = (await Task.WhenAll(task1)).ToList();
+
+        Day4.Challenge2 chal2 = new Challenge2();
+        var task2 = Task.Run(() => chal2.Solve(inputList));
+        var results = (await Task.WhenAll(task2)).ToList();
 
         return results;
     }
