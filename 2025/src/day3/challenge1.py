1268e9e7069b102c484408daadbc5c9fc691ad71
Andrew Palmer <ahpalmer72@gmail.com>
2026-01-07 13:39:38 -0500

    d3c1 done

--- 2025/src/day3/challenge1.py
+++ 2025/src/day3/challenge1.py
@@ -2,9 +2,30 @@ import utility.retrieve_data as data_util
 
 def challenge1(day_file: str = "day3"):
     data: list[str] = data_util.retrieve_data_from_file_name(day_file)
-    # TODO: Implement solution
-    print(data)
+    total_count = 0
+    for line in data:
+        line_clean = line.replace('\n', '')
+        total_count += find_largest_total_number(line_clean)
+    print(total_count)
     return None
+
+def find_largest_total_number(line: str) -> int:
+        largest_index: int = find_biggest_number_index(create_line_array_without_last_digit(line))
+        line_array: list[int] = [int(char) for char in line]
+        leftover_array = line_array[largest_index + 1:]
+        second_largest_index: int = find_biggest_number_index(leftover_array)
+        first_digit = str(line_array[largest_index])
+        second_digit = str(leftover_array[second_largest_index])
+        return int(first_digit + second_digit)
+
+def find_biggest_number_index(line_array: list[int]) -> int:
+    largest = max(line_array)
+    return line_array.index(largest)
+
+def create_line_array_without_last_digit(line: str) -> list[int]:
+    line_array: list[int] = [int(char) for char in line]
+    line_array.pop()
+    return line_array
 
 if __name__ == "__main__":
     challenge1()
--- 2025/tests/test_day3.py
+++ 2025/tests/test_day3.py
@@ -1,9 +1,25 @@
 import day3.challenge1 as d3c1
 import day3.challenge2 as d3c2
 
-def test_day3_challenge1():
-    # TODO: Add tests
-    pass
+def test_d3c1_find_largest_total_num():
+    line1 = "987654321111111"
+    line2 = "811111111111119"
+    line3 = "234234234234278"
+    line4 = "818181911112111"
+    assert d3c1.find_largest_total_number(line1) == 98
+    assert d3c1.find_largest_total_number(line2) == 89
+    assert d3c1.find_largest_total_number(line3) == 78
+    assert d3c1.find_largest_total_number(line4) == 92
+
+def test_d3c1_find_biggest_number_index():
+    line1 = "987654321111111"
+    line2 = "811111111111119"
+    line3 = "234234234234278"
+    line4 = "818181911112111"
+    assert d3c1.find_biggest_number_index(d3c1.create_line_array_without_last_digit(line1)) == 0
+    assert d3c1.find_biggest_number_index(d3c1.create_line_array_without_last_digit(line2)) == 0
+    assert d3c1.find_biggest_number_index(d3c1.create_line_array_without_last_digit(line3)) == 13
+    assert d3c1.find_biggest_number_index(d3c1.create_line_array_without_last_digit(line4)) == 6
 
 def test_day3_challenge2():
     # TODO: Add tests
