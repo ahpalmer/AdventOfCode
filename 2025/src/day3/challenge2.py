b574134e0e20da67a3ade85a9efb7e98abd16e4f
Andrew Palmer <ahpalmer72@gmail.com>
2026-01-11 10:14:52 -0500

    Finished d3c2

--- 2025/src/day3/challenge2.py
+++ 2025/src/day3/challenge2.py
@@ -2,6 +2,8 @@ import utility.retrieve_data as data_util
 
 def challenge2(day_file: str = "day3"):
     data: list[str] = data_util.retrieve_data_from_file_name(day_file)
+    for i in range(len(data)):
+        data[i] = data[i].strip()
     total_count = 0
     for line in data:
         total_count += solution_loop(line)
@@ -9,13 +11,15 @@ def challenge2(day_file: str = "day3"):
     return None
 
 def solution_loop(line: str) -> int:
-    digits_deleted = 0
+    digits_remaining = len(line)
     loop_counter = 0
-    line_length_original = len(line)
-    while (digits_deleted < 3 and loop_counter < len(line) - 1):
-        line, loop_counter, temp_digits_deleted = compare_numbers(line, loop_counter)
-        digits_deleted += temp_digits_deleted
-    return int(line[:line_length_original - 3])
+    while (digits_remaining > 12 and loop_counter < len(line) - 1):
+        line, loop_counter, digits_deleted = compare_numbers(line, loop_counter)
+        digits_remaining = digits_remaining - digits_deleted
+    print(f"Final line: {line}")
+    if digits_remaining > 12:
+        print(f"MORE THAN 12 DIGITS: {line[0:12]}")
+    return int(line[0:12])
 
 def compare_numbers(line: str, loop_counter: int) -> tuple[str, int, int]:
     if (line[loop_counter] < line[loop_counter + 1]):
