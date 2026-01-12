64822a86187ccfc9d1411b8a92459418da84ff3b
Andrew Palmer <ahpalmer72@gmail.com>
2026-01-12 18:36:00 -0500

    Added day 4 data and instructions

---
+++ 2025/tests/test_day4.py
@@ -0,0 +1,10 @@
+import day4.challenge1 as d4c1
+import day4.challenge2 as d4c2
+
+def test_day4_challenge1():
+    # TODO: Add tests
+    pass
+
+def test_day4_challenge2():
+    # TODO: Add tests
+    pass
